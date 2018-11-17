925111820943a182b50e96e7fce032c12809566a
Emmanouil Kampitakis <info+git@kampitakis.de>
2018-11-18 00:19:04 +0100

    Expand "installation" script

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ICS2EDIR=/opt/ics2entropiawiki
-VENVDIR=/opt/ics2entropiawiki-venv
+VENVDIR=/opt/ics2entropiawiki/venv
 CONFDIR=/etc/ics2entropiawiki
 
 mkdir $VENVDIR 
 virtualenv -p /usr/bin/python3 $VENVDIR
 
 cp ics2entropiawiki.py $ICS2EDIR
-cp sample_config.ini $CONFDIR
+cp sample_config.ini $CONFDIR/config.ini
 
 $VENVDIR/bin/pip3 install -r requirements.txt
+
+cp systemd/* /etc/systemd/system/
+systemd deamon-reload
